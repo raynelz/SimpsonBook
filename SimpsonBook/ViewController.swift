4283dfa680763b09a8cd0341088164cd11137743
Zahar Litvinchuk <97597336+raynelz@users.noreply.github.com>
2023-09-03 00:49:27 +0300

    Initial Commit

---
+++ SimpsonBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpsonBook
+//
+//  Created by Захар Литвинчук on 03.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
